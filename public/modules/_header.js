10fd91ae29213eca4e481d8e496964e32547bbb7
ray.zhu <278227578@qq.com>
2016-07-22 13:09:37 +0800

    add keydown event for search.

--- public/modules/_header.js
+++ public/modules/_header.js
@@ -5,4 +5,11 @@ define('_header', ['jquery', 'util'], function ($, util) {
             location.href = '/search/'+keys;
         }
     });
+    $('.head-search .keys').on('keydown', function (e) {
+        var keys;
+
+        if( (e.keyCode === 13 || e.which === 13) && (keys = $('.head-search > .keys').val())){
+            location.href = '/search/'+keys;
+        }
+    });
 });
